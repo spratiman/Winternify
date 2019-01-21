5ab78c08ed11d7311f0a6e64c8937f4098981784
spratiman <spratiman@yahoo.com>
2019-01-21 03:42:10 -0500

    Add pre-populated data for setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: 'pratiman@winternify.com', password: 'summer2019', password_confirmation: 'summer2019')
+Product.create(title: 'Toronto Raptors Jersey', price: 40.0, inventory_count: 5, inStock: true)
+Product.create(title: 'Toronto Maple Leafs Jersey', price: 50.0, inventory_count: 3, inStock: true)
+Product.create(title: 'Toronto FC Jersey', price: 45.0, inventory_count: 0, inStock: false)
